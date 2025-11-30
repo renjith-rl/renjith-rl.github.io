897881e6c52315be0c4c49e38345e1e1d84516f5
renjith-rl <renjithrl8086@gmail.com>
2025-11-30 17:49:32 +0530

    Add files via upload
    old lite theamed

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -2,18 +2,17 @@ window.addEventListener('load', function() {
   let pbar = document.getElementById('bar');
   let pct = 0;
   function animateBar() {
-    pct += Math.random()*14+12;
+    pct += Math.random()*13+12;
     if (pct > 100) pct = 100;
     pbar.style.width = pct + '%';
-    if (pct < 100) setTimeout(animateBar, 80);
+    if (pct < 100) setTimeout(animateBar, 85);
     else {
-      // Fade out loader on finish
       setTimeout(function() {
         document.getElementById('preloader').style.opacity = '0';
         setTimeout(function() {
           document.getElementById('preloader').style.display = 'none';
-        }, 700); // Remove after fade
-      }, 200);
+        }, 620);
+      }, 250);
     }
   }
   animateBar();
